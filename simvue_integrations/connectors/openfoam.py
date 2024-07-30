3b026802281fe7a396110bb1176fd95ed5f21e67
Matthew Field <matthew.field@ukaea.uk>
2024-07-30 16:45:34 +0100

    Added docstrings for MOOSE and Openfoam connectors

--- simvue_integrations/connectors/openfoam.py
+++ simvue_integrations/connectors/openfoam.py
@@ -55,6 +55,21 @@ class OpenfoamRun(WrappedRun):
 
     @mp_tail_parser.log_parser
     def _log_parser(self, file_content: str, **__) -> tuple[dict[str,typing.Any], dict[str, typing.Any]]:
+        """Parses information from any Openfoam log file, and uploads key data to Simvue.
+
+        Uploads information from the header of the file as Metadata to the Run, uploads log messages from the file
+        which are produced before the solve begins as Events, and then uploads residuals values as Metrics.
+
+        Parameters
+        ----------
+        file_content : str
+            The latest additions to the file.
+
+        Returns
+        -------
+        tuple[dict[str,typing.Any], dict[str, typing.Any]]
+            An (empty) dictionary of metadata, and a dictionary of any metrics obtained from the file.
+        """
         
         exp1: re.Pattern[str] = re.compile(
             "^(.+):  Solving for (.+), Initial residual = (.+), Final residual = (.+), No Iterations (.+)$"
@@ -131,6 +146,8 @@ class OpenfoamRun(WrappedRun):
         return {}, metrics
 
     def pre_simulation(self):
+        """Uploads inputs from the system, constant and 0 directories, and adds the Openfoam process.
+        """
         super().pre_simulation()
 
         self.metadata_uploaded = False
@@ -152,14 +169,16 @@ class OpenfoamRun(WrappedRun):
         )
     
     def during_simulation(self):
+        """Tracks any log files produced by Openfoam."""
         # Track all log files
-            self.file_monitor.tail(
-                parser_func=self._log_parser,
-                path_glob_exprs=[os.path.join(self.openfoam_case_dir, "log.*")],
-                callback=lambda *_, **__: None
-            )
+        self.file_monitor.tail(
+            parser_func=self._log_parser,
+            path_glob_exprs=[os.path.join(self.openfoam_case_dir, "log.*")],
+            callback=lambda *_, **__: None
+        )
 
     def post_simulation(self):
+        """Uploads all results found in the Openfoam case directory."""
         reg_exp = re.compile("([\d\.]+)")
         result_dirs = [dir_name for dir_name in os.listdir(self.openfoam_case_dir) if reg_exp.match(dir_name)]
         self._save_directory(result_dirs, "results.zip", "output")     
@@ -171,6 +190,17 @@ class OpenfoamRun(WrappedRun):
         upload_as_zip: bool = True,
         openfoam_env_vars: typing.Optional[typing.Dict[str, typing.Any]] = None
         ):
+        """Command to launch the Openfoam simulation and track it with Simvue.
+
+        Parameters
+        ----------
+        openfoam_case_dir : pydantic.DirectoryPath
+            The path to the directory containing the openfoam case (containing an Allrun file, and input directories)
+        upload_as_zip : bool, optional
+            Whether to upload inputs and outputs as zip files, by default True
+        openfoam_env_vars : typing.Optional[typing.Dict[str, typing.Any]], optional
+            A dictionary of any environment variables to pass to the Openfoam simulation, by default None
+        """
         self.openfoam_case_dir = openfoam_case_dir
         self.upload_as_zip = upload_as_zip
         self.openfoam_env_vars = openfoam_env_vars or {}
