81e7a4693ef9e318f1afafa2e6dbb612c5db4701
Matt Field <matthew.field@ukaea.uk>
2024-08-13 15:18:12 +0100

    Fixed bug with n-threads

--- simvue_integrations/extras/create_command.py
+++ simvue_integrations/extras/create_command.py
@@ -11,17 +11,24 @@ def format_command_env_vars(cmd_kwargs: typing.Dict[str, typing.Union[bool, str,
     """
     cmd_list = []
     for kwarg, val in cmd_kwargs.items():
-        _quoted_val: str = f'"{val}"'
+        kwarg = kwarg.strip('-')
         if len(kwarg) == 1:
             if isinstance(val, bool) and val:
                 cmd_list += [f"-{kwarg}"]
             else:
-                cmd_list += [f"-{kwarg}{(' '+ _quoted_val) if val else ''}"]
+                cmd_list += [f"-{kwarg}", str(val)]
         else:
             kwarg = kwarg.replace("_", "-")
             if isinstance(val, bool) and val:
                 cmd_list += [f"--{kwarg}"]
             else:
-                cmd_list += [f"--{kwarg}{(' '+_quoted_val) if val else ''}"]
+                # This is ridiculous, but for some reason for the specific option '--n-threads', MOOSE will only accept formatting
+                # like '--n-threads=4', and does not recognise '--n-threads 4'. According to the docstring of the command line tool,
+                # all other arguments work correctly with the '--a b' formatting, just not that one!
+                # Could change them all to be formatted as '--a=b', but then mpiexec doesn't recognise any arguments provided in that form >:(
+                if kwarg == "n-threads":
+                    cmd_list += [f"--{kwarg}={val}"]
+                else:
+                    cmd_list += [f"--{kwarg}", str(val)]
                 
     return cmd_list
