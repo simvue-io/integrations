b7b83b2c2f737413ce83f2c7f387d3f21d48e255
Matt Field <matthew.field@ukaea.uk>
2024-10-23 16:51:05 +0100

    Updated fds integration test

--- tests/integration/fds/test_fds_connector.py
+++ tests/integration/fds/test_fds_connector.py
@@ -1,5 +1,6 @@
 from examples.fds.minimal_fds import fds_example
-import pytest
+import pathlib
+import tempfile
 import simvue
 
 def test_fds_connector(check_fds_setup, folder_setup):
@@ -9,20 +10,39 @@ def test_fds_connector(check_fds_setup, folder_setup):
     
     client = simvue.Client()
     run_data = client.get_run(run_id)
+    events = [event["message"] for event in client.get_events(run_id)]
     
     # Check run description and tags from init have been added
+    assert run_data["description"] == "An example of using the FDSRun Connector to track an FDS simulation."
+    assert run_data["tags"] == ['fds', 'vents']
     
     # Check alert has been added
+    assert "visibility_below_three_metres" in [alert["alert"]["name"] for alert in run_data["alerts"]]
     
     # Check metadata from header
+    assert run_data["metadata"]["fds.mpi_processes"] == 1
     
     # Check metadata from input file
+    assert run_data["metadata"]["_grp_devc_1.id"] == "flow_volume_supply"
     
     # Check events from log
+    assert "Time Step: 1, Simulation Time: 0.092 s" in events
     
-    # Check events and metadata from CTRL log
-    
+    # Check events from DEVC/CTRL log
+    assert "DEVC 'timer' has been set to 'True' at time 2s" in [event["message" for event in events]]
+        
     # Check metrics from HRR file
+    assert run_data["metrics"]["HRR"]["count"] > 0
     
     # Check metrics from DEVC file
+    assert run_data["metrics"]["flow_volume_supply"]["last"] == -1.5
+
+    temp_dir = tempfile.TemporaryDirectory
     
+    # Check input file uploaded as input
+    client.get_artifacts_as_files(run_id, "input", temp_dir.name)
+    assert pathlib.Path(temp_dir.name).joinpath("activate_vents.fds").exists()
+    
+    # Check results uploaded as output
+    client.get_artifacts_as_files(run_id, "output", temp_dir.name)
+    assert pathlib.Path(temp_dir.name).joinpath("supply_exhaust_vents.smv").exists()
